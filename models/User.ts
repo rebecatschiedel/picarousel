ca9d1df3009abf1389428a5ebe151d9c39309fea
Samuel Tschiedel <mukaobr@gmail.com>
2021-04-11 19:48:34 -0300

    style: prettier

--- models/User.ts
+++ models/User.ts
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-const findOrCreate = require('mongoose-findorcreate');
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    email: {
-        type: String,
-    },
-    password: {
-        type: String,
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    googleId: {
-        type: String
-    },
-    facebookId: {
-        type: String
-    },
-    favoritePhotos: {
-        type: Array
-    }
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  googleId: {
+    type: String,
+  },
+  facebookId: {
+    type: String,
+  },
+  favoritePhotos: {
+    type: Array,
+  },
 });
 
-UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 UserSchema.plugin(findOrCreate);
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
